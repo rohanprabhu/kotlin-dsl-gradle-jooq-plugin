d26b6a3362d33d2840cb174a483a91f04c5734f0
Rohan Prabhu <rohan@rohanprabhu.com>
2017-12-20 04:29:47 +0530

    Ready for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,18 @@
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+
+    dependencies {
+        classpath("com.gradle.publish:plugin-publish-plugin:0.9.9")
+    }
+}
+
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     `maven-publish`
+    id("com.gradle.plugin-publish") version "0.9.9"
 }
 
 group = "com.rohanprabhu"
@@ -16,13 +27,29 @@ gradlePlugin {
     }
 }
 
+pluginBundle {
+    website = "https://github.com/rohanprabhu/kotlin-dsl-gradle-jooq-plugin"
+    vcsUrl = "https://github.com/rohanprabhu/kotlin-dsl-gradle-jooq-plugin"
+    description = "jOOQ plugin aimed for use with kotlin-dsl gradle projects"
+    tags = listOf("jooq", "kotlin-dsl")
+
+    (plugins) {
+        "kotlinDslJooqPlugin" {
+            id = "com.rohanprabhu.kotlin-dsl-jooq"
+            displayName = "jOOQ plugin for Kotlin DSL"
+        }
+    }
+}
+
 val jooqVersion by project
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
+    gradleApi()
     compile("org.jooq:jooq:$jooqVersion")
     compile("org.jooq:jooq-meta:$jooqVersion")
     compile("org.jooq:jooq-codegen:$jooqVersion")
