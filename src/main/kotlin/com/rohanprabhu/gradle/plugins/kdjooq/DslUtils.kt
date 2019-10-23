861a7cdc69618ace76dd150829efa9517501f5db
Rohan Prabhu <rohan@rohanprabhu.com>
2019-10-23 11:08:16 +0530

    Update README, add remaining configuration properties and rename config block functions

--- src/main/kotlin/com/rohanprabhu/gradle/plugins/kdjooq/DslUtils.kt
+++ src/main/kotlin/com/rohanprabhu/gradle/plugins/kdjooq/DslUtils.kt
@@ -5,15 +5,25 @@ import org.jooq.meta.jaxb.Target
 import org.jooq.util.xml.jaxb.*
 
 class ForcedTypesListOps(
-        list : List<ForcedType>,
-        private val internalList: MutableList<ForcedType> = list.toMutableList()
+    private val database: Database
 ) {
     fun forcedType(configure: ForcedType.() -> Unit) : Unit = ForcedType().apply(configure).let {
-        internalList.add(it)
+        database.forcedTypes.add(it)
     }
+}
 
-    internal fun getList() =
-        internalList.toList()
+class EnumTypesListOps(
+    private val database: Database
+) {
+    fun enumType(configure: EnumType.() -> Unit) : Unit = EnumType().apply(configure).let {
+        database.enumTypes.add(it)
+    }
+}
+
+class JdbcPropertiesOps(
+    private val jdbc: Jdbc
+) {
+    fun property(configure: Property.() -> Unit) = Property().apply(configure)
 }
 
 fun jooqCodegenConfiguration(
@@ -21,63 +31,47 @@ fun jooqCodegenConfiguration(
 ) : Configuration =
     Configuration().apply(configure)
 
-fun Configuration.jdbc(configure: Jdbc.() -> Unit) : Jdbc = Jdbc().apply(configure).also { this.jdbc = it }
-
-fun Configuration.generator(configure: Generator.() -> Unit) : Generator =
-        Generator().apply(configure).also {
-            it.database = Database()
-            this.generator = it
-        }
-
-fun Generator.target(configure: Target.() -> Unit) : Target = Target().apply(configure).also { this.target = it }
-
-fun Generator.database(configure: Database.() -> Unit) : Database =
-        Database().apply(configure).also { this.database = it}
-
-fun Generator.strategy(configure: Strategy.() -> Unit) : Strategy = Strategy().apply(configure).also {
-    this.strategy = it
+fun jdbcConfig(configure: Jdbc.() -> Unit) = Jdbc().apply(configure)
+fun Configuration.jdbc(configure: Jdbc.() -> Unit) {
+    jdbcConfig(configure).also { this.jdbc = it }
 }
 
-fun Generator.generate(configure: Generate.() -> Unit) : Generate = Generate().apply(configure).also {
-    this.generate = it
+fun Jdbc.properties(configure: JdbcPropertiesOps.() -> Unit) = JdbcPropertiesOps(this).apply(configure)
+
+fun generatorConfig(configure: Generator.() -> Unit) = Generator().apply(configure)
+fun Configuration.generator(configure: Generator.() -> Unit) {
+    generatorConfig(configure).also {
+        it.database = Database()
+        this.generator = it
+    }
 }
 
-fun MutableList<ForcedType>.forcedType(configure: ForcedType.() -> Unit) : ForcedType =
-        ForcedType().apply(configure).also {
-            this.add(it)
-        }
-
-fun Configuration.enumType(configure: EnumType.() -> Unit) : EnumType = EnumType().apply(configure)
-
-fun Configuration.dbProperty(configure: Property.() -> Unit) : Property = Property().apply(configure)
-
-fun Jdbc.dbProperty(key: String, value: String) = Property().apply { this.key = key; this.value = value }
-
-/*
-fun Database.schema(configure: Schema.() -> Unit) : Schema = Schema().apply(configure).also {
-    this.schema = it
-}
- */
-
-fun Configuration.catalog(configure: Catalog.() -> Unit) : Catalog = Catalog().apply(configure)
-
-fun Database.forcedTypes(vararg forcedType: ForcedType) = forcedType.toMutableList().also {
-    this.forcedTypes = it
+fun targetConfig(configure: Target.() -> Unit) = Target().apply(configure)
+fun Generator.target(configure: Target.() -> Unit) {
+    targetConfig(configure).also { this.target = it }
 }
 
-fun Configuration.forcedTypes(configure: ForcedTypesListOps.() -> Unit) : List<ForcedType> =
-    ForcedTypesListOps(emptyList()).apply(configure).getList()
+fun databaseConfig(configure: Database.() -> Unit) = Database().apply(configure)
+fun Generator.database(configure: Database.() -> Unit) {
+    databaseConfig(configure).also { this.database = it }
+}
 
-operator fun Jdbc?.invoke(configure: Jdbc.() -> Unit) : Jdbc = (this ?: Jdbc()).apply(configure)
-operator fun Generator?.invoke(configure: Generator.() -> Unit) : Generator = (this ?: Generator()).apply(configure)
-operator fun Target?.invoke(configure: Target.() -> Unit) : Target = (this ?: Target()).apply(configure)
-operator fun Database?.invoke(configure: Database.() -> Unit) : Database = (this ?: Database()).apply(configure)
-operator fun Generate?.invoke(configure: Generate.() -> Unit) : Generate = (this ?: Generate()).apply(configure)
-operator fun EnumType?.invoke(configure: EnumType.() -> Unit) : EnumType = (this ?: EnumType()).apply(configure)
-operator fun Property?.invoke(configure: Property.() -> Unit) : Property = (this ?: Property()).apply(configure)
-operator fun Schema?.invoke(configure: Schema.() -> Unit) : Schema = (this ?: Schema()).apply(configure)
-operator fun Catalog?.invoke(configure: Catalog.() -> Unit) : Catalog = (this ?: Catalog()).apply(configure)
-operator fun List<ForcedType>?.invoke(configure: ForcedTypesListOps.() -> Unit) : List<ForcedType> =
-    ForcedTypesListOps((this ?: emptyList())).apply(configure).getList()
-operator fun JooqCodeGenerationTask.invoke(configure: JooqCodeGenerationTask.() -> Unit) : JooqCodeGenerationTask =
-    this.apply(configure)
+fun strategyConfig(configure: Strategy.() -> Unit) = Strategy().apply(configure)
+fun Generator.strategy(configure: Strategy.() -> Unit) {
+    strategyConfig(configure).also {
+        this.strategy = it
+    }
+}
+
+fun generateConfig(configure: Generate.() -> Unit) = Generate().apply(configure)
+fun Generator.generate(configure: Generate.() -> Unit) {
+    generateConfig(configure).also {
+        this.generate = it
+    }
+}
+
+fun forcedTypeConfig(configure: ForcedType.() -> Unit) = ForcedType().apply(configure)
+fun Database.forcedTypes(configure: ForcedTypesListOps.() -> Unit) = ForcedTypesListOps(this).apply(configure)
+
+fun enumTypeConfig(configure: EnumType.() -> Unit) = EnumType().apply(configure)
+fun Database.enumTypes(configure: ForcedTypesListOps.() -> Unit) = ForcedTypesListOps(this).apply(configure)
